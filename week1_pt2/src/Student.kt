ebc7764f2a953b5c9356bb177013f7f1d3bf0950
Ewan Williams <ewan@ewan.name>
2024-10-03 13:41:44 +0100

    Week 1 tasks complete

--- week1_pt2/src/Student.kt
+++ week1_pt2/src/Student.kt
@@ -1,6 +1,6 @@
 class Student(val id: String, val name: String, val course: String, val mark: Double) {
 
     override fun toString(): String {
-        return "ID: $id | Name: $name | Course $course | Mark: $mark"
+        return "ID: $id | Name: $name | Course: $course | Mark: $mark"
     }
 }
--- week1_pt2/src/main.kt
+++ week1_pt2/src/main.kt
@@ -1,12 +1,14 @@
 fun main() {
     for (i in 1..3) {
-        val id = i
+        print("ID: ")
+        val id = readlnOrNull().toString()
         print("Name: ")
-        val name = readLine()
+        val name = readlnOrNull().toString()
         print("Course: ")
-        val course = readLine()
+        val course = readlnOrNull().toString()
         print("Mark: ")
-        val mark = readLine()?.toDouble()
-
+        val mark = readLine()?.toDouble() ?: 0.0
+        val new_student = Student(id, name, course, mark)
+        println(new_student)
     }
 }
