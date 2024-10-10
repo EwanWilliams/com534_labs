178faa93045294368e620f2fa0db87486f7849ab
Ewan Williams <ewan@ewan.name>
2024-10-10 13:43:27 +0100

    Week 3 task 1 complete

--- week1_pt2/src/main.kt
+++ week1_pt2/src/main.kt
@@ -1,4 +1,19 @@
 fun main() {
+    /* week 1 stuff?
+    for (i in 1..3) {
+        print("ID: ")
+        val id = readlnOrNull().toString()
+        print("Name: ")
+        val name = readlnOrNull().toString()
+        print("Course: ")
+        val course = readlnOrNull().toString()
+        print("Mark: ")
+        val mark = readlnOrNull()?.toDouble()
+        val new_student = Student(id, name, course, mark)
+        println(new_student)
+    }
+    */
+    /* week 2 shit
     var test_undergrad = Undergraduate("001", "Ewan Williams", "Software Engineering")
     println(test_undergrad)
     print("Enter student mark: ")
@@ -11,4 +26,20 @@ fun main() {
     test_masters.mark = readln().toDouble()
     println(test_masters.getGrade())
     println(test_masters.didPass())
+    */
+
+    //week 3 shit pt1
+    var students = mutableListOf<Student>()
+    for (i in 1..3) {
+        val id = i.toString()
+        print("Name: ")
+        val name = readlnOrNull().toString()
+        print("Course: ")
+        val course = readlnOrNull().toString()
+        students.add(Undergraduate(id, name, course))
+    }
+
+    for (student in students) {
+        println(student)
+    }
 }
