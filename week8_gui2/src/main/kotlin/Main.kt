865f60d49497fc16a0d77c6001fbfef3723a24cb
Ewan Williams <ewan@ewan.name>
2024-11-14 14:32:43 +0000

    week 8 pt 2 done

--- week8_gui2/src/main/kotlin/Main.kt
+++ week8_gui2/src/main/kotlin/Main.kt
@@ -12,13 +12,20 @@ import kotlinx.coroutines.newSingleThreadContext
 @Composable
 @Preview
 fun App() {
-    val students = remember { mutableStateListOf<Student>() }
-
+    val uni = University()
+    val displayStudents = remember { mutableStateOf(listOf<Student>()) }
 
     MaterialTheme {
         Column {
-            AddStudent(onStudentAdded = { students.add(it) })
-            StudentList(students.toList())
+            AddStudent(onStudentAdded = {
+                uni.addStudent(it)
+                displayStudents.value = uni.studentList.toList()
+            })
+            SearchByCourse(onCourseSearch = { course ->
+                val studentsOnCourse = uni.studentList.filter { student -> student.course == course }
+                displayStudents.value = studentsOnCourse
+            })
+            StudentList(displayStudents.value)
         }
     }
 }
@@ -60,6 +67,23 @@ fun StudentList(students: List<Student>) {
         students.forEach {
             Text(it.toString())
         }
+    }
+}
+
+@Composable
+fun SearchByCourse(onCourseSearch: (String) -> Unit) {
+    val searchQuery = remember { mutableStateOf<String>("") }
+
+    Column {
+        TextField(searchQuery.value,
+            onValueChange = { searchQuery.value = it },
+            singleLine = true,
+            label = { Text("Enter course:") }
+        )
+        Button(onClick = {
+            onCourseSearch(searchQuery.value)
+            searchQuery.value = ""
+        } ) { Text("Search") }
     }
 }
 
